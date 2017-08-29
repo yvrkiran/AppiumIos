330c1c6307a7b4183b227eea81bf0c5b36dcb007
Ravi Kiran <yvrkiran@users.noreply.github.com>
2017-08-29 11:03:07 -0500

    Ios device automation
    initial commit

---
+++ src/main/java/com/selenium/appium/Safari.java
@@ -0,0 +1,32 @@
+package com.selenium.appium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
+
+public class Safari {
+
+	
+	public static AppiumDriver driver;
+	
+	public static void main(String[] args) throws MalformedURLException {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("deviceName","iphone Simulator");
+		capabilities.setCapability("platformName","iOS");
+		capabilities.setCapability("platformVersion","10.3");
+		capabilities.setCapability("browserName","safari");
+		
+		driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("https://bryant.pprd.goalquestprogram.com/");
+
+	}
+
+}
